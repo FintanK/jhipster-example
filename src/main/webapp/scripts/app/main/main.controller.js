46245ac4e2289690a5afebf6cf43d2c686e279a6
Fintan Kearney <dev@Fintans-MacBook-Pro.local>
2015-04-26 01:24:50 +0100

    Messing around with entities.

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('cmsApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Post, Principal, ParseLinks) {
+
+        $scope.posts = [];
+
+        Post.query({page: $scope.page, per_page: 20}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                $scope.posts = result;
+        });
+
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
--- src/main/webapp/scripts/components/language/language.service.js
+++ src/main/webapp/scripts/components/language/language.service.js
@@ -28,6 +28,20 @@ angular.module('cmsApp')
  */
     .constant('LANGUAGES', [
         'en', 'fr'
+        ,'ca'
+        ,'zh-cn'
+        ,'zh-tw'
+        ,'da'
+        ,'de'
+        ,'hu'
+        ,'ja'
+        ,'kr'
+        ,'pl'
+        ,'pt-br'
+        ,'ru'
+        ,'es'
+        ,'sv'
+        ,'tr'
         //JHipster will add new languages here
     ]
 );
